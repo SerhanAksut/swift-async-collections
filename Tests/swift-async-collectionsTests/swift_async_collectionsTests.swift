04963f69cde93212158a8572c8a82cc6b753924b
Serhan Aksüt <serhanaksut@gmail.com>
2026-03-09 18:40:24 +0100

    Initial Commit

---
+++ Tests/swift-async-collectionsTests/swift_async_collectionsTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import swift_async_collections
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
